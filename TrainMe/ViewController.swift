ccf588a16c546e3c819cc83d29a09d8e8e551382
Hod <hod.gohasi@hotmail.com>
2020-06-12 08:08:25 +0100

    Initial Commit

---
+++ TrainMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainMe
+//
+//  Created by user165563 on 6/12/20.
+//  Copyright © 2020 hodgohasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
