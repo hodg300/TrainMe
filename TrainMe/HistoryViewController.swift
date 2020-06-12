74ea520b1e6553d064a748ea41b5111ab9364f58
Hod <hod.gohasi@hotmail.com>
2020-06-12 12:39:05 +0100

    * Adding controllers

---
+++ TrainMe/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  TrainMe
+//
+//  Created by user165563 on 6/12/20.
+//  Copyright © 2020 hodgohasi. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
