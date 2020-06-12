788d499a94c2536365717291397375a3ed9d89b2
Hod <hod.gohasi@hotmail.com>
2020-06-12 11:52:26 +0100

    * Start final project *Connecting to firebase with google login

--- TrainMe/AppDelegate.swift
+++ TrainMe/AppDelegate.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import FirebaseUI
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    
+    func application(_ app: UIApplication, open url: URL,
+                         options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+            let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+      }
+      // other URL handling goes here.
+      return false
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -31,6 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
